4fd086decac62dd6c75bd1d4ec665a0da03e4ff5
adfmb <actuario.alfredo.mb@gmail.com>
2020-11-22 16:01:07 -0600

    update df_calif_atms()

--- utils/df_calif_atms.R
+++ utils/df_calif_atms.R
@@ -1,7 +1,7 @@
 df_calif_atms<-function(df,df_campos,camposllave){
   df_00<-df%>%
     select(one_of(c(camposllave,df_campos$variable)))
-  df_calif_atms<-df_00%>%
+  df_calif<-df_00%>%
     mutate(
       calif=as.numeric(t(as.matrix(select(.,one_of(df_campos$variable)))%*%matrix(df_campos$aporte_var,nrow = nrow(df_campos))))
     )%>%
@@ -13,5 +13,5 @@ df_calif_atms<-function(df,df_campos,camposllave){
   
   # length(unique(df$atm))
   # nrow(df)
-  return(df_calif_atms)
+  return(df_calif)
 }
