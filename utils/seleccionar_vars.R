5e3cb697ddd969e8ffa2822184d306b42ce8290d
adfmb <actuario.alfredo.mb@gmail.com>
2020-11-22 14:54:02 -0600

    update seleccionar_vars()

--- utils/seleccionar_vars.R
+++ utils/seleccionar_vars.R
@@ -1,4 +1,4 @@
-seleccionar_vars<-function(df,camposllave,vars_excluir=NULL){
+seleccionar_vars<-function(df,camposllave,vars_excluir=NULL,numcols=3){
   campos01<-names(df)[!names(df)%in%camposllave]
   campos02<-campos01[!campos01%in%vars_excluir]
   campos03<-campos02[!grepl("_p_20",campos02)]
@@ -13,11 +13,24 @@ seleccionar_vars<-function(df,camposllave,vars_excluir=NULL){
   campos03<-campos03[!grepl("_p_98",campos03)]
   campos03<-campos03[!grepl("_p_99",campos03)]
   campos03<-campos03[!grepl("_p_100",campos03)]
-  campos03<-campos03[!grepl("Giro_",campos03)]
   
-  return(campos03)
+  campos<-campos03[!grepl("Giro_",campos03)]
+  
+  num_campos03<-length(campos)
+  vec_nums<-c()
+  for(i in 1:numcols){
+    # i<-3
+    numer<-ifelse(length(vec_nums)==0,num_campos03,num_campos03-sum(vec_nums))
+    denom<-numcols-length(vec_nums)
+    num_i<-ifelse(i<numcols,ceiling(numer/denom),numer)
+    vec_nums<-c(vec_nums,num_i)
+  }
+  
+
+    
+  return(list("campos"=campos,"vec_nums"=vec_nums))
   
 }
 
-camposllave<-c("atm","Division","Giro","Estado","Ciudad","CP","Del.Muni","Colonia","Latitud","Longitud","cvemun")
-seleccionar_vars(df,camposllave)
+# camposllave<-c("atm","Division","Giro","Estado","Ciudad","CP","Del.Muni","Colonia","Latitud","Longitud","cvemun")
+# camposvariables<-seleccionar_vars(df,camposllave,numcols = 3)
